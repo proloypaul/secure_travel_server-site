b033c7392789cb3148f217ac621892d89c22e4b6
proloypaul <proloyp23@gmail.com>
2022-01-27 13:04:42 +0600

    connent with mongodb

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require('express')
+const { MongoClient } = require('mongodb');
 const app = express()
+require('dotenv').config()
 const port = process.env.PORT || 3600;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e3dsx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+console.log(uri)
+async function run() {
+    try {
+      await client.connect();
+      
+      
+     
+    } finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send("secure travel server running")
